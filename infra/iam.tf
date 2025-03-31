25cd8f3cc631515364112bd793bac3ecb12ec8bf
sbldevnet <sbldevnet@gmail.com>
2025-03-31 20:29:55 +0200

    wip

---
+++ infra/iam.tf
@@ -0,0 +1,1 @@
+// TODO: create policy and users
---
+++ infra/variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  description = "AWS region where resources will be deployed."
+  type        = string
+  default     = "eu-west-1"
+}
+
+variable "s3_name" {
+  description = "S3 bucket name."
+  type        = string
+  default     = "awsugvlc-apps-config"
+}
+
+variable "db_name" {
+  description = "DynamoDB table name."
+  type        = string
+  default     = "catalog"
+}
+
+variable "lambda_name" {
+  description = "Lambda function name."
+  type        = string
+  default     = "logs-exporter"
+}
